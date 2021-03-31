169dc208a4b1a6b7b1337c5e1f2f6adae1493515
Marcus Filipsson <macke2002@outlook.com>
2021-03-31 09:22:10 +0200

    soon done

--- tcp client/Form1.cs
+++ tcp client/Form1.cs
@@ -6,25 +6,40 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
 using System.Windows.Forms;
+
 
 namespace tcp_client
 {
     public partial class Form1 : Form
     {
+        TcpClient klient;
+        int port = 12345;
+        public KlientForm()...
+        private void btnSägHej_Click( object sender, EventArgs e)
+        
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
+       
 
         private void button1_Click(object sender, EventArgs e)
         {
+            IPAddress adress = IPAdress.Parse(tbxIPAdress.Text);
+            klient = new TcpClient();
+            klient.NoDelay = true;
+            klient.Connect(adress, port);
 
+            if (klient.Connected)
+            { 
         }
+        byte[] utData = Encoding.Unicode.GetBytes("Hej!");
+        Klient.GetStream().Write(utData, 0, utData.Lenght);
+        Klient.Close();
+            }
     }
 }
